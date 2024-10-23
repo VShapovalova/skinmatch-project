9fe59e8a7c65b151dd645c7002f639d9cf092ea2
VictoriaShap <shapovalova.viktoriia@student.uzhnu.edu.ua>
2024-10-23 09:36:34 +0300

    Improved husky

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
+import styles from './App.css';
+
 const App = () => {
-    return (
-        <div> My First App</div>
-    )
-}
+    return <div className={styles.main}> My First React App</div>;
+};
 
 export default App;
